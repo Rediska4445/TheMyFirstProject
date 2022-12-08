046b7096244ee20acbb3e0e4e49e33e815fcd42a
Rediska4445 <113374478+Rediska4445@users.noreply.github.com>
2022-12-08 21:41:12 +0300

    Update 0.1
    #1: Исправлена ошибка регистрации (теперь нельзя будет зарегистрировать два аккаунта с одинаковыми именами);
    #2: Добавлена прослушка даты регистрации (теперь будет производится запись даты каждой регистрации с отдельный файл);

--- Reg.java
+++ Reg.java
@@ -7,29 +7,53 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
 import java.util.Scanner;
 
-public class Reg{
-	public static void main(String[] args) {
-        String nameReg;
-        String passReg;      	
-		Scanner scanner = new Scanner(System.in);			
-		System.out.println("Имя");
-		if(scanner.hasNextLine()) {	
-		String RegName = scanner.nextLine();
-		System.out.println("Пароль");
-		String RegPass = scanner.nextLine();
-        try(FileWriter writer = new FileWriter("DBUsers.txt", true))
-        {
-            nameReg = RegName;
-            passReg = RegPass;
-            writer.write(nameReg + " " +  passReg + '\n');
-            writer.flush();
-            System.out.println("Вы успешно зарегистрировались");
-			} catch(IOException ex){
-          System.out.println(ex.getMessage());
+public class reg {
+	
+    static String nameReg;
+    static String passReg;
+    static Date date = new Date();
+    
+		public static void reg() throws IOException, InterruptedException {  	
+			Scanner scanner = new Scanner(System.in);			
+			System.out.println("Имя для регистрации: ");
+			if(scanner.hasNextLine()) {	
+			String RegName = scanner.nextLine();
+			System.out.println("Пароль для регистрации: ");
+			String RegPass = scanner.nextLine();
+			File file = new File("DBUsers.txt");
+	        Scanner sc = new Scanner(file);
+	        BufferedReader reader = new BufferedReader(new FileReader("DBUsers.txt"));
+	        while(sc.hasNextLine()){    	        	
+	            String Read = (sc.nextLine());    
+	            boolean isContains = Files
+	                    .lines(Paths.get("DBUsers.txt"))
+	                    .anyMatch(e->e.contains("Имя: " + RegName));
+	            if(isContains == true) {
+	            	System.out.println("Упс, твоё самое оригинальное имя, неожиданно зарегистрировали, перерегистрируйся");
+	            	Thread.sleep(500);
+	        		reg();
+	            } 
+	        }
+	        try(FileWriter writer = new FileWriter("DBUsers.txt", true))
+	        {
+	            nameReg = RegName;
+	            passReg = RegPass;
+	            writer.write('\n' + "Имя: " + nameReg + " Пароль: " + passReg);
+	            writer.flush();
+	            System.out.println("Вы успешно зарегистрировались");
+	            auth.auth();
+				} catch(IOException ex){
+	          System.out.println(ex.getMessage());
+			}
+	        FileWriter writer = new FileWriter("DBUsersRegDate.txt", true);
+	            writer.write(String.format(nameReg + " - Время и дата регистрации: %tc", date) + '\n');
+	            writer.flush();
 		}
-	    }
-	} 
-   }
+	}
 }
+
